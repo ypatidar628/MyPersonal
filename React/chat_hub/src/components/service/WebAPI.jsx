cc30cfa55bbfb39be9030c07ba37ab3653ff4029
ypatidar628 <patidaryogendra43@gmail.com>
2024-12-03 17:57:51 +0530

    some changes in chat-hub/userPost

--- React/chat_hub/src/components/service/WebAPI.jsx
+++ React/chat_hub/src/components/service/WebAPI.jsx
@@ -2,5 +2,5 @@ export default{
     registerAPI:'http://tutorials.codebetter.in:7084/auth/save',
     loginAPI:'http://tutorials.codebetter.in:7084/auth/login',
     allUserListAPI:'http://tutorials.codebetter.in:7084/api/user/list',
-
+    allUserPostAPI:'http://tutorials.codebetter.in:7084/api/post/list'
 }
--- React/chat_hub/src/components/user/MyPost.jsx
+++ React/chat_hub/src/components/user/MyPost.jsx
@@ -1,4 +1,5 @@
-function MyPost(){
+ function MyPost(){
+    
     return <div className="container">
         <h1 style={{textAlign:'center', color:"Purple" ,textShadow:'3px 3px 3px black'}}>My Post Called...</h1>
     </div>
